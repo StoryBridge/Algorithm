75d52ddeee18c8118b616116a49cbb107017a29a
이성현 <zzennam@naver.com>
2019-05-14 17:27:29 +0900

    Create some_list.py

---
+++ codeit/some_list/some_list.py
@@ -0,0 +1,20 @@
+# 파라미터 some_list를 거꾸로 뒤집는 함수
+def flip(some_list):
+    # 코드를 입력하세요.
+    # base case
+    if len(some_list) < 2:
+        return some_list
+    # Recursive case
+    # some_list[-1]은 배열의 마지막값
+    else:
+        temp = some_list[-1:]        
+        temp.append(flip(some_list[:-1]))
+        
+        return temp
+        
+    
+
+# 테스트
+some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+some_list = flip(some_list)
+print(some_list)
