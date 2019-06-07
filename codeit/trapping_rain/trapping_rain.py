597eb7bc33b07a9187d4f35f201155ca5c1e644c
이성현 <zzennam@naver.com>
2019-06-07 22:02:32 +0900

    Create trapping_rain.py
    못풀었음.. 나중에 한번 다시 풀어볼것

---
+++ codeit/trapping_rain/trapping_rain.py
@@ -0,0 +1,25 @@
+def trapping_rain(buildings):
+    # 코드를 작성하세요
+    # 총 담기는 빗물의 양을 변수에 저장
+    total_rain = 0
+
+    # 리스트의 각 인덱스를 돌면서 해당 칸에 담기는 빗물의 양을 구한다
+    # 0번 인덱스와 마지막 인덱스는 볼 필요 없다.
+    for i in range(1, len(buildings) - 1):
+        # 현재 인덱스를 기준으로 양쪽에 가장 높은 건물의 위치를 구한다
+        max_left = max(buildings[:i])
+        max_right = max(buildings[i:])
+
+        # 현재 인덱스에 빗물이 잠길 수 있는 높이
+        upper_bound = min(max_left, max_right)
+
+        # 현재 인덱스에 담기는 빗물의 양을 계산
+        # 만약 upper_bound가 현재 인덱스 건물보다 높지 않다면, 현재 인덱스에 담기는 빗물은 -
+        total_rain += max(0, upper_bound - buildings[i])
+
+    return total_rain
+
+
+# 테스트
+# print(trapping_rain([3, 0, 0, 2, 0, 4]))
+print(trapping_rain([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
