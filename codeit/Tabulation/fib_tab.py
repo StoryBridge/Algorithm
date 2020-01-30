f3ab8e47c985d5aaa46d3376224173084c69e174
이성현 <zzennam@naver.com>
2020-01-30 18:42:34 +0900

    Create fib_tab.py

---
+++ codeit/Tabulation/fib_tab.py
@@ -0,0 +1,12 @@
+def fib_tab(n):
+    fib_table = [0, 1, 1]
+     
+    for i in range(3, n+3):
+        fib_table.append(fib_table[i-1] + fib_table[i-2])         
+    
+    return fib_table[n]
+
+# Test
+print(fib_tab(10))
+print(fib_tab(56))
+print(fib_tab(132))
